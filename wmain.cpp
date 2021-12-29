3a60daed409bdbe4455ad72e099e9e351f56fc80
uemuraj <uemura.junya@gmail.com>
2021-12-29 11:19:41 +0900

    テスト用にデバッグコンソールに出力を送る。

--- wmain.cpp
+++ wmain.cpp
@@ -1,5 +1,8 @@
+#include <windows.h>
+
 #include <locale>
 #include <iostream>
+#include <sstream>
 
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.Devices.Bluetooth.Advertisement.h>
@@ -18,7 +21,16 @@ int wmain(int argc, wchar_t * argv[], wchar_t * envp[])
 	BluetoothLEAdvertisementWatcher watcher;
 	watcher.ScanningMode(BluetoothLEScanningMode::Active);
 	watcher.AllowExtendedAdvertisements(true);
-	watcher.Received([=](auto, auto args) { std::wcout << L"---" << std::endl << args << std::endl; });
+	watcher.Received([=](auto, auto args) {
+	#if 0
+		std::wstringstream wss;
+		wss << L"---" << std::endl << args << std::endl;
+		::OutputDebugString(wss.str().c_str());
+	#else
+		std::wcout << L"---" << std::endl << args << std::endl;
+	#endif
+		}
+	);
 
 	std::wcerr << L"Hit enter to stop." << std::endl;
 	watcher.Start();
